51da7df4c54b98d881bc4f09c34e55d984bd8e8c
yechanhan <80242550+yechanhan@users.noreply.github.com>
2021-03-18 21:08:04 +0900

    Add files via upload

---
+++ pointer-202131422 한예찬.cpp
@@ -0,0 +1,16 @@
+﻿// pointer-202131422 한예찬.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main() {
+    int a = 35;
+
+    int* ptr_a;
+    ptr_a = &a;
+
+    printf("a의 값 : %d\n", a);
+    printf("a의 주솟값 : %d\n", &a);
+    printf("ptr_a에 저장된 값 : %d\n", ptr_a);
+    printf("ptr_a가 가르키는 변수의 값 : %d\n", *ptr_a);
+}
